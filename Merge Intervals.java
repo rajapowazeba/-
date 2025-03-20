6869432df06ea1916053ccc9a6f79cd1e59c8a17
rajapowazeba <rajapowazeba@gmail.com>
2025-03-20 10:04:53 +0500

    Лабораторная работа №2

---
+++ Merge Intervals.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        List<int[]> result = new ArrayList<>();
+        int[] current = intervals[0];
+        result.add(current);
+        for (int[] interval : intervals) {
+            if (interval[0] <= current[1]) {
+                current[1] = Math.max(current[1], interval[1]);
+            } else {
+                current = interval;
+                result.add(current);
+            }
+        }
+        return result.toArray(new int[result.size()][]);
+    }
+}
